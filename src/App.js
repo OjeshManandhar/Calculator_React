faf3bb7a0749124e20f79016d74e4926b1aaa733
OjeshManandhar <ozes.manandhar@gmail.com>
2019-02-04 21:06:33 +0545

    Add display box

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div class="App">
+				<table>
+					<thead>
+						<textarea id="display" rows="1" cols="20" value="0" readOnly></textarea>
+					</thead>
+				</table>
+			</div>
+		);
+	} 
 }
 
 export default App;
