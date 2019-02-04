d0481f01173d483f1043c99c8261740d498669ef
OjeshManandhar <ozes.manandhar@gmail.com>
2019-02-04 21:46:54 +0545

    Add props validation in Button.js

--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Button extends Component {
 	render() {
@@ -7,5 +8,9 @@ class Button extends Component {
 		);
 	}
 }
+
+Button.propTypes = {
+	children: PropTypes.string.isRequired
+};
 
 export default Button;
